e053ef41a0c5795346045ff80695027befab3a38
Richard Brearton <richardbrearton@gmail.com>
2024-12-03 20:32:24 +0000

    Added a simple column_names mod

--- core/utils/src/lib.rs
+++ core/utils/src/lib.rs
@@ -1,3 +1,7 @@
+pub mod dataframes;
+pub mod functions;
+pub mod well_known_terms;
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
---
+++ core/utils/src/well_known_terms/column_names.rs
@@ -0,0 +1,6 @@
+/// This module defines constant strings that are used as column names.
+
+pub const X: &str = "x";
+pub const Y: &str = "y";
+pub const Z: &str = "z";
+pub const F: &str = "f";
