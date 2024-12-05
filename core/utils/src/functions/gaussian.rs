91c90cc4a5f923e7a358a01b62c27b54469170fd
Richard Brearton <richardbrearton@gmail.com>
2024-12-05 21:24:59 +0000

    Added a simple Gaussian function

--- core/utils/src/functions/gaussian.rs
+++ core/utils/src/functions/gaussian.rs
@@ -1,1 +1,15 @@
+/// An implementation of a simple Gaussian function.
+/// This function takes in the x-coordinate, the mean, the standard deviation, and an optional
+/// prefactor. If the prefactor is not provided, the function will, by default, make sure that the
+/// Gaussian is normalized.
+pub fn gaussian(x_coords: f64, mean: f64, std_dev: f64, prefactor: Option<f64>) -> f64 {
+    // Check to see if we were given the prefactor.
+    let prefactor = match prefactor {
+        Some(value) => value,
+        None => 1.0 / (std_dev * (2.0 * std::f64::consts::PI).sqrt()),
+    };
 
+    // Evaluate the Gaussian, multiplying by the appropriate prefactor.
+    let b = -0.5 * ((x_coords - mean) / std_dev).powi(2);
+    prefactor * b.exp()
+}
