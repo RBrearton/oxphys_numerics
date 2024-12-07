757e051bbe2e166dce8c4364cf3bc3e73470b6e2
Richard Brearton <richardbrearton@gmail.com>
2024-12-07 17:24:44 +0000

    Added more tests; made arithmetic more general and removed some panics

--- core/utils/src/enums/scalar_type.rs
+++ core/utils/src/enums/scalar_type.rs
@@ -15,9 +15,20 @@ macro_rules! impl_arith_trait {
 
             fn $method(self, other: Self) -> Self {
                 match (self, other) {
-                    (ScalarType::F64(left), ScalarType::F64(right)) => ScalarType::F64(left $op right),
-                    (ScalarType::F32(left), ScalarType::F32(right)) => ScalarType::F32(left $op right),
-                    _ => panic!("Cannot perform operation on two different scalar types."),
+                    (ScalarType::F64(left), ScalarType::F64(right)) => {
+                        ScalarType::F64(left $op right)
+                    }
+                    (ScalarType::F32(left), ScalarType::F32(right)) => {
+                        ScalarType::F32(left $op right)
+                    }
+
+                    // If the types are different, convert to the higher precision type.
+                    (ScalarType::F32(left), ScalarType::F64(right)) => {
+                        ScalarType::F64(f64::from(left) $op right)
+                    }
+                    (ScalarType::F64(left), ScalarType::F32(right)) => {
+                        ScalarType::F64(left $op f64::from(right))
+                    }
                 }
             }
         }
@@ -68,6 +79,62 @@ impl ScalarType {
             }
         }
     }
+
+    pub fn sqrt(self) -> Self {
+        match self {
+            ScalarType::F64(value) => ScalarType::F64(value.sqrt()),
+            ScalarType::F32(value) => ScalarType::F32(value.sqrt()),
+        }
+    }
+
+    pub fn sin(self) -> Self {
+        match self {
+            ScalarType::F64(value) => ScalarType::F64(value.sin()),
+            ScalarType::F32(value) => ScalarType::F32(value.sin()),
+        }
+    }
+
+    pub fn cos(self) -> Self {
+        match self {
+            ScalarType::F64(value) => ScalarType::F64(value.cos()),
+            ScalarType::F32(value) => ScalarType::F32(value.cos()),
+        }
+    }
+
+    pub fn exp(self) -> Self {
+        match self {
+            ScalarType::F64(value) => ScalarType::F64(value.exp()),
+            ScalarType::F32(value) => ScalarType::F32(value.exp()),
+        }
+    }
+
+    pub fn log(self, base: Self) -> Self {
+        match (self, base) {
+            (ScalarType::F64(inner), ScalarType::F64(base)) => ScalarType::F64(inner.log(base)),
+            (ScalarType::F32(inner), ScalarType::F32(base)) => ScalarType::F32(inner.log(base)),
+            // If the types are different, convert to the higher precision type.
+            (ScalarType::F32(inner), ScalarType::F64(base)) => {
+                ScalarType::F64(f64::from(inner).log(base))
+            }
+            (ScalarType::F64(inner), ScalarType::F32(base)) => {
+                ScalarType::F64(inner.log(f64::from(base)))
+            }
+        }
+    }
+
+    pub fn ln(self) -> Self {
+        match self {
+            ScalarType::F64(value) => ScalarType::F64(value.ln()),
+            ScalarType::F32(value) => ScalarType::F32(value.ln()),
+        }
+    }
+
+    pub fn abs(self) -> Self {
+        match self {
+            ScalarType::F64(value) => ScalarType::F64(value.abs()),
+            ScalarType::F32(value) => ScalarType::F32(value.abs()),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -85,6 +152,14 @@ mod tests {
             ScalarType::F32(1.0) + ScalarType::F32(2.0),
             ScalarType::F32(3.0)
         );
+        assert_eq!(
+            ScalarType::F32(1.0) + ScalarType::F64(2.0),
+            ScalarType::F64(3.0)
+        );
+        assert_eq!(
+            ScalarType::F64(1.0) + ScalarType::F32(2.0),
+            ScalarType::F64(3.0)
+        );
     }
 
     #[test]
@@ -96,6 +171,14 @@ mod tests {
         assert_eq!(
             ScalarType::F32(5.0) - ScalarType::F32(3.0),
             ScalarType::F32(2.0)
+        );
+        assert_eq!(
+            ScalarType::F32(5.0) - ScalarType::F64(3.0),
+            ScalarType::F64(2.0)
+        );
+        assert_eq!(
+            ScalarType::F64(5.0) - ScalarType::F32(3.0),
+            ScalarType::F64(2.0)
         );
     }
 
@@ -109,6 +192,14 @@ mod tests {
             ScalarType::F32(2.0) * ScalarType::F32(3.0),
             ScalarType::F32(6.0)
         );
+        assert_eq!(
+            ScalarType::F32(2.0) * ScalarType::F64(3.0),
+            ScalarType::F64(6.0)
+        );
+        assert_eq!(
+            ScalarType::F64(2.0) * ScalarType::F32(3.0),
+            ScalarType::F64(6.0)
+        );
     }
 
     #[test]
@@ -121,12 +212,14 @@ mod tests {
             ScalarType::F32(6.0) / ScalarType::F32(2.0),
             ScalarType::F32(3.0)
         );
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_add_different_types() {
-        let _ = ScalarType::F64(1.0) + ScalarType::F32(2.0);
+        assert_eq!(
+            ScalarType::F32(6.0) / ScalarType::F64(2.0),
+            ScalarType::F64(3.0)
+        );
+        assert_eq!(
+            ScalarType::F64(6.0) / ScalarType::F32(2.0),
+            ScalarType::F64(3.0)
+        );
     }
 
     #[test]
@@ -153,6 +246,60 @@ mod tests {
             ScalarType::F64(2.0).powf(ScalarType::F32(3.0)),
             ScalarType::F64(8.0)
         );
+    }
+
+    #[test]
+    fn test_sqrt() {
+        assert_eq!(ScalarType::F64(4.0).sqrt(), ScalarType::F64(2.0));
+        assert_eq!(ScalarType::F32(4.0).sqrt(), ScalarType::F32(2.0));
+    }
+
+    #[test]
+    fn test_sin() {
+        // These tests are pretty weak.
+        assert_eq!(ScalarType::F64(0.0).sin(), ScalarType::F64(0.0));
+        assert_eq!(ScalarType::F32(0.0).sin(), ScalarType::F32(0.0));
+    }
+
+    #[test]
+    fn test_cos() {
+        // These tests are pretty weak.
+        assert_eq!(ScalarType::F64(0.0).cos(), ScalarType::F64(1.0));
+        assert_eq!(ScalarType::F32(0.0).cos(), ScalarType::F32(1.0));
+    }
+
+    #[test]
+    fn test_exp() {
+        // These tests are pretty weak.
+        assert_eq!(ScalarType::F64(0.0).exp(), ScalarType::F64(1.0));
+        assert_eq!(ScalarType::F32(0.0).exp(), ScalarType::F32(1.0));
+    }
+
+    #[test]
+    fn test_log() {
+        assert_eq!(
+            ScalarType::F64(8.0).log(ScalarType::F64(2.0)),
+            ScalarType::F64(3.0)
+        );
+        assert_eq!(
+            ScalarType::F32(8.0).log(ScalarType::F32(2.0)),
+            ScalarType::F32(3.0)
+        );
+        assert_eq!(
+            ScalarType::F32(8.0).log(ScalarType::F64(2.0)),
+            ScalarType::F64(3.0)
+        );
+        assert_eq!(
+            ScalarType::F64(8.0).log(ScalarType::F32(2.0)),
+            ScalarType::F64(3.0)
+        );
+    }
+
+    #[test]
+    fn test_ln() {
+        // These tests are pretty weak.
+        assert_eq!(ScalarType::F64(1.0).ln(), ScalarType::F64(0.0));
+        assert_eq!(ScalarType::F32(1.0).ln(), ScalarType::F32(0.0));
     }
 
     #[test]
