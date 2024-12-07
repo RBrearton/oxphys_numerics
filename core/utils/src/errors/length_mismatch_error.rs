a50dc181a5827bc9897f9d8d11314f69633dc88d
Richard Brearton <richardbrearton@gmail.com>
2024-12-07 19:18:18 +0000

    Added a LengthMismatchError struct

---
+++ core/utils/src/errors/length_mismatch_error.rs
@@ -0,0 +1,32 @@
+use std::{collections::HashMap, fmt};
+
+#[derive(Debug)]
+pub struct LengthMismatchError {
+    details: String,
+}
+
+impl LengthMismatchError {
+    /// Create a new LengthMismatchError.
+    /// # Arguments
+    /// * `map` - A mapping from variable names to their lengths.
+    fn new(map: HashMap<&str, u64>) -> LengthMismatchError {
+        LengthMismatchError {
+            details: format!(
+                "The lengths of the variables in the expression do not match: {:?}",
+                map
+            ),
+        }
+    }
+}
+
+impl fmt::Display for LengthMismatchError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "LengthMismatchError: {}", self.details)
+    }
+}
+
+impl std::error::Error for LengthMismatchError {
+    fn description(&self) -> &str {
+        &self.details
+    }
+}
--- core/utils/src/lib.rs
+++ core/utils/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod enums;
+pub mod errors;
 pub mod functions;
 pub mod structs;
 pub mod traits;
