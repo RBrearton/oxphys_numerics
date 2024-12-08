0971cf2492e023ef939bdcf342a5547a0b7a1c3c
Richard Brearton <richardbrearton@gmail.com>
2024-12-08 17:18:24 +0000

    Started thinking about the main package namespace

--- src/oxphys_numerics/__init__.py
+++ src/oxphys_numerics/__init__.py
@@ -1,1 +1,20 @@
 """The home of the oxphys_numerics python api."""
+
+from .models import Add, Constant, Cos, Div, Exp, Ln, Log, Mul, Negate, Sin, Sqrt, Sub, Variable
+
+__all__ = [
+    "Add",
+    "Constant",
+    "Cos",
+    "Div",
+    "Exp",
+    "ExprType",
+    "Ln",
+    "Log",
+    "Mul",
+    "Negate",
+    "Sin",
+    "Sqrt",
+    "Sub",
+    "Variable",
+]
