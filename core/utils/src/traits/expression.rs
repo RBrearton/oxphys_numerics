cf0ccd5d2642b923caefb1894c1423d2f98c84ca
Richard Brearton <richardbrearton@gmail.com>
2024-12-08 00:24:56 +0000

    Removed get_variables from Expression trait

--- core/utils/src/traits/expression.rs
+++ core/utils/src/traits/expression.rs
@@ -1,16 +1,8 @@
-use std::collections::HashMap;
-
-use crate::enums::scalar_type::ScalarType;
-
 /// # Expression
 /// This defines everything that we expect from our data structures that represent mathematical
 /// expressions.
 pub trait Expression {
     /// # Evaluate
     /// Run the expression with the given variables and return the result.
-    fn evaluate(&self, variables: &HashMap<String, ScalarType>) -> ScalarType;
-
-    /// # Get variables
-    /// Returns a vector of the names of all the variables in the expression.
-    fn get_variables(&self) -> Vec<String>;
+    fn evaluate(&self, variables: &Vec<f64>) -> f64;
 }
