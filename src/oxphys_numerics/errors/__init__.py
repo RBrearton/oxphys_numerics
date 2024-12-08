a95fd85f6c1ee565ebdff78e66ceccc4370c1007
Richard Brearton <richardbrearton@gmail.com>
2024-12-08 16:29:01 +0000

    Added missing `__all__`

--- src/oxphys_numerics/errors/__init__.py
+++ src/oxphys_numerics/errors/__init__.py
@@ -1,4 +1,9 @@
 """All exceptions defined by the `oxphys_numerics` package are defined in this package."""
 
-from ._invalid_expression import InvalidExpressionError
+from ._invalid_expression_error import InvalidExpressionError
 from ._oxphys_numerics_error import OxphysNumericsError
+
+__all__ = [
+    "InvalidExpressionError",
+    "OxphysNumericsError",
+]
