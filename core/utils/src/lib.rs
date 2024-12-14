0a80e3e4307dc79b72113f94385ef5b2fa6581d0
Richard Brearton <richardbrearton@gmail.com>
2024-12-14 21:51:49 +0000

    Removed inbuilt meaningless tests

--- core/utils/src/lib.rs
+++ core/utils/src/lib.rs
@@ -4,18 +4,3 @@ pub mod functions;
 pub mod structs;
 pub mod traits;
 pub mod well_known_terms;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
