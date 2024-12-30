4d1f127e853c1d61bfc249e50da2933d6d71ade0
Richard Brearton <richardbrearton@gmail.com>
2024-12-30 14:17:20 +0000

    Started thinking of initialization state of expressions

--- core/oxphys_numerics/src/enums/mod.rs
+++ core/oxphys_numerics/src/enums/mod.rs
@@ -1,6 +1,8 @@
 pub mod binary_node;
 pub mod expr;
+pub mod initialized_expression;
 pub mod leaf_node;
 pub mod scalar_type;
 pub mod unary_node;
+pub mod uninitialized_expression;
 pub mod variable;
