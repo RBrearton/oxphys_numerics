8287b11bb5b221e93fb1d7fcc9947f1e0bf37049
Richard Brearton <richardbrearton@gmail.com>
2025-01-03 21:41:34 +0000

    Added some simple implementations to UninitializedLeaf

--- core/oxphys_numerics/src/enums/uninitialized_leaf.rs
+++ core/oxphys_numerics/src/enums/uninitialized_leaf.rs
@@ -1,5 +1,7 @@
 use crate::structs::uninitialized_variable::UninitializedVariable;
 
+use super::initialized_leaf::InitializedLeaf;
+
 /// # Uninitialized leaf
 /// A leaf node associated with metadata that the user has input, but it hasn't yet been initialized
 /// and therefore can't yet be used to evaluate expressions.
@@ -7,4 +9,24 @@ use crate::structs::uninitialized_variable::UninitializedVariable;
 pub enum UninitializedLeaf {
     Constant(f64),                   // Leaf node: a constant value.
     Variable(UninitializedVariable), // The usize is the index of the variable in the input vector.
+}
+
+impl UninitializedLeaf {
+    /// # Initialize
+    /// Returns an initialized leaf node.
+    pub fn initialize(self, index: usize) -> InitializedLeaf {
+        unimplemented!()
+    }
+
+    /// # New variable
+    /// Create a new variable leaf node.
+    pub fn new_variable(name: String) -> Self {
+        UninitializedLeaf::Variable(UninitializedVariable::new(name))
+    }
+
+    /// # New constant
+    /// Create a new constant leaf node.
+    pub fn new_constant(value: f64) -> Self {
+        UninitializedLeaf::Constant(value)
+    }
 }
