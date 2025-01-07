6f656b5a26d324b1bdf7ea5f47564af308b6168a
Richard Brearton <richardbrearton@gmail.com>
2025-01-07 21:14:51 +0000

    Added missing `is_initialized` argument

--- core/oxphys_numerics/src/traits/expression_node.rs
+++ core/oxphys_numerics/src/traits/expression_node.rs
@@ -3,5 +3,5 @@ use crate::enums::expr::Expr;
 pub trait ExpressionNode {
     /// # To expr
     /// Convert this expression node to an `Expr`.
-    fn to_expr(&self) -> Expr;
+    fn to_expr(&self, is_initialized: bool) -> Expr;
 }
