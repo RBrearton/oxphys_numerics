f326907b98dd62ad645890a866fc0f04ba39caf1
Richard Brearton <richardbrearton@gmail.com>
2025-01-07 21:17:58 +0000

    Added another to_expr method

--- core/oxphys_numerics/src/enums/leaf_node.rs
+++ core/oxphys_numerics/src/enums/leaf_node.rs
@@ -1,4 +1,7 @@
-use super::{initialized_leaf::InitializedLeaf, uninitialized_leaf::UninitializedLeaf};
+use super::{
+    expr::Expr, initialized_expr::InitializedExpr, initialized_leaf::InitializedLeaf,
+    uninitialized_expr::UninitializedExpr, uninitialized_leaf::UninitializedLeaf,
+};
 
 /// # LeafExpr
 /// An enum that represents the different types of leaf expressions that can be used in the
@@ -9,4 +12,17 @@ use super::{initialized_leaf::InitializedLeaf, uninitialized_leaf::Uninitialized
 pub enum LeafNode {
     Uninitialized(UninitializedLeaf), // An uninitialized leaf node.
     Initialized(InitializedLeaf),     // An initialized leaf node.
+}
+
+impl LeafNode {
+    pub fn to_expr(self) -> Expr {
+        match self {
+            LeafNode::Uninitialized(uninitialized) => {
+                Expr::Uninitialized(UninitializedExpr::Leaf(uninitialized))
+            }
+            LeafNode::Initialized(initialized) => {
+                Expr::Initialized(InitializedExpr::Leaf(initialized))
+            }
+        }
+    }
 }
