da3f4ffa70ee8ca79fa2730dd300adec4da409a3
Richard Brearton <richardbrearton@gmail.com>
2025-01-07 22:00:32 +0000

    Removed totally unused modules

--- core/oxphys_numerics/src/enums/mod.rs
+++ core/oxphys_numerics/src/enums/mod.rs
@@ -1,12 +1,10 @@
 pub mod binary_node;
 pub mod expr;
 pub mod initialized_expr;
-pub mod initialized_expression;
 pub mod initialized_leaf;
 pub mod leaf_node;
 pub mod scalar_type;
 pub mod unary_node;
 pub mod uninitialized_expr;
-pub mod uninitialized_expression;
 pub mod uninitialized_leaf;
 pub mod variable;
